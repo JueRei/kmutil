2ada929e0dc229548e12c598f24f7286100b3fa6
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-17 20:43:09 +0100

    mountDir with user/password, mingw printFile()

---
+++ src/mingwX64Main/kotlin/KmFileUtilMingw.kt
@@ -0,0 +1,31 @@
+package de.rdvsb.kmutil
+
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.toDuration
+
+/** mount a dirName already defined in /etc/fstab
+ *
+ * check if it is already mounted if not unmount it at end of script via onExitRunCmds list
+ * @return mountStatus: Boolean
+ */
+@OptIn(ExperimentalTime::class)
+public actual fun mountDir(dirName: String, userName: String, password: String): Boolean {
+	var ok = false
+	system("mount | grep \"on $dirName \"", 5.toDuration(DurationUnit.SECONDS)) { line, lineFrom, _ ->
+		val msgId = if (lineFrom == LineFrom.OUT) 'I' else 'E'
+		logMessage(msgId, "mountDir1> $line")
+		if (line.contains("on $dirName ")) ok = true
+	}
+	if (ok) return true // dirName is already mounted
+
+	ok = false
+	system("mount $dirName; sleep 1; mount | grep \"on $dirName \"", 300.toDuration(DurationUnit.SECONDS)) { line, lineFrom, _ ->
+		val msgId = if (lineFrom == LineFrom.OUT) 'I' else 'E'
+		logMessage(msgId, "mountDir2> $line")
+		if (line.contains("on $dirName ")) ok = true
+	}
+	if (ok) onExitRunCmds.add("umount $dirName")
+
+	return ok
+}
---
+++ src/mingwX64Main/kotlin/KmProcMingw.kt
@@ -0,0 +1,63 @@
+package de.rdvsb.kmutil
+
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import de.rdvsb.kmapi.*
+
+/**
+ * run a binary app with args
+ * quoted args are supported (exactly like shell argument quoting)
+ * no scripts supported no arg globbing done
+ * @return the called process exit value
+ */
+@OptIn(ExperimentalTime::class)
+public actual fun runBinary(cmd: String, timeout: Duration): Int {
+	TODO("Not yet implemented")
+}
+
+/**
+ * run via shell so globbing and command grouping or chaining with semicolons is supported
+ * stdin is inherited from current process
+ * stderr and stdout are captured and a supplied callback function is called for each captured stdin/stderr line
+ *
+ * **Note**: the processLine callback is called concurrently from two different threads
+ * @return the called process exit value
+ */
+@OptIn(ExperimentalTime::class)
+public actual fun system(cmd: String, timeout: Duration, processLine: (line: String, lineFrom: LineFrom, process: KmProcess) -> Unit): Int {
+	TODO("Not yet implemented")
+}
+
+/**
+ * run via shell so globbing and command grouping or chaining with semicolons is supported
+ * stdin is inherited from current process
+ * stderr and stdout are written to current handles
+ * @return the called process exit value
+ */
+@OptIn(ExperimentalTime::class)
+public actual fun system(cmd: String, timeout: Duration): Int {
+	TODO("Not yet implemented")
+}
+
+public actual class GlobalLock actual constructor(private val lockName: String, private val doLock: Boolean) : Closeable {
+
+	public actual fun isLocked(): Boolean {
+		TODO("Not yet implemented")
+	}
+
+	public actual fun lock(): GlobalLock {
+		TODO("Not yet implemented")
+	}
+
+	public actual fun tryLock(): GlobalLock? {
+		TODO("Not yet implemented")
+	}
+
+	public actual fun unlock(): GlobalLock {
+		TODO("Not yet implemented")
+	}
+
+	public actual override fun close() {
+		TODO("Not yet implemented")
+	}
+}
