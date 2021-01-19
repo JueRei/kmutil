e5f05b68a86df92564b841213a0b672be9ae6d06
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-19 11:59:54 +0100

    typo

--- src/jvmMain/kotlin/KmStringJvm.kt
+++ src/jvmMain/kotlin/KmStringJvm.kt
@@ -4,7 +4,7 @@ import java.text.NumberFormat
 import java.util.*
 
 // enforce en_US locale. Always use consistent decimal point and grouping chars for system logs
-/private val locale = Locale.setDefault(Locale("en", "US")).run { Locale.getDefault() }
+private val locale = Locale.setDefault(Locale("en", "US")).run { Locale.getDefault() }
 
 
 public actual val kmStringFmtGroupingFlag: Char = ','
