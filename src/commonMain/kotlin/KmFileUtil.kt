b82fd743a712081be6132a1399bb44c64c45d5b9
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-25 21:33:58 +0100

    File.rotateRename() moved to KmFile.kt File.rotateRename() old log name pattern changed to <baseName>#NN.<ext>

--- src/commonMain/kotlin/KmFileUtil.kt
+++ src/commonMain/kotlin/KmFileUtil.kt
@@ -3,6 +3,7 @@ package de.rdvsb.kmutil
 import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.toDuration
+import de.rdvsb.kmapi.*
 
 /** mount a dirName already defined in /etc/fstab
  *
@@ -11,3 +12,23 @@ import kotlin.time.toDuration
  */
 @OptIn(ExperimentalTime::class)
 public expect fun mountDir(dirName: String, userName: String = "", password: String = ""): Boolean
+
+public fun File.rotateRename(lastOldVersion: Int = 9): Unit {
+	var path = absolutePath
+	var pathBase = path
+	var ext = ""
+	path.lastIndexOf('.').let {
+		if (it >= 0) {
+			pathBase = path.substring(0, it)
+			ext = path.substring(it)
+		}
+	}
+
+	for (n in lastOldVersion downTo 0) {
+		val olderFile = File("$pathBase#${n.toFmt("%02d")}$ext")
+		val newerFile = if (n == 0) File(path) else  File("$pathBase#${(n-1).toFmt("%02d")}$ext")
+
+		olderFile.exists() && olderFile.delete()
+		newerFile.exists() && newerFile.renameTo(olderFile)
+	}
+}
--- src/commonMain/kotlin/kmUtil.kt
+++ src/commonMain/kotlin/kmUtil.kt
@@ -3,12 +3,3 @@ package de.rdvsb.kmutil
 import de.rdvsb.kmapi.*
 
 
-public fun File.rotateRename(lastOldVersion: Int = 9): Unit {
-	var path = absolutePath
-	for (n in lastOldVersion downTo 0) {
-		val olderFile = File("${path}.${n.toFmt("%02d")}")
-		val newerFile = if (n == 0) File(path) else  File("${path}.${(n-1).toFmt("%02d")}")
-		olderFile.delete()
-		newerFile.renameTo(olderFile)
-	}
-}
