56529f100967c7684814dafd29391ebcbfff9db8
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-26 10:22:41 +0100

    added copyright note

--- src/commonMain/kotlin/KmArgs.kt
+++ src/commonMain/kotlin/KmArgs.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2021 Jürgen Reichmann, Jettingen, Germany
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package de.rdvsb.kmutil
 
 import de.rdvsb.kmapi.File
